dd5b291dd0510cf54c340d0d7f6141b6a2abafbf
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-02-08 07:01:12 -0500

    update FileService with month mapping

--- src/com/coderscampus/FileService.java
+++ src/com/coderscampus/FileService.java
@@ -4,20 +4,30 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
 public class FileService {
+	
+	private static final Map<String, Integer> MONTH_MAP = new HashMap<>();
 
+    static {
+        MONTH_MAP.put("Jan", 1);
+        MONTH_MAP.put("Feb", 2);
+        MONTH_MAP.put("Mar", 3);
+        MONTH_MAP.put("Apr", 4);
+        MONTH_MAP.put("May", 5);
+        MONTH_MAP.put("Jun", 6);
+        MONTH_MAP.put("Jul", 7);
+        MONTH_MAP.put("Aug", 8);
+        MONTH_MAP.put("Sep", 9);
+        MONTH_MAP.put("Oct", 10);
+        MONTH_MAP.put("Nov", 11);
+        MONTH_MAP.put("Dec", 12);
+    }
+	
 	public static Map<LocalDate, Integer> readCSVFile(String fileName) {
 		Map<LocalDate, Integer> monthlySales = new HashMap<>();
-		DateTimeFormatter formatter = new DateTimeFormatterBuilder()
-				.parseCaseInsensitive()
-				.appendPattern("MMM-yy")
-				.toFormatter(Locale.ENGLISH);
 		
 		try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
 			reader.lines()
@@ -25,8 +35,12 @@ public class FileService {
 				  .map(line -> line.split(","))
 				  .forEach(fields -> {
 					  String dateString = fields[0];
-					  System.out.println("Date String: " + dateString);
-					  LocalDate date = LocalDate.parse(fields[0], formatter);
+					  
+					  String[] dateParts = dateString.split("-");
+					  int month = MONTH_MAP.get(dateParts[0]);
+					  int year = Integer.parseInt("20" + dateParts[1]);
+					  
+					  LocalDate date = LocalDate.of(year, month, 1);
 					  int sales = Integer.parseInt(fields[1]);
 					  
 					  monthlySales.merge(date, sales, Integer::sum);
